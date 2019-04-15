0088d5ae7310df3a8ea7ba03f4ed0224de026056
rajdip34 <rajdipmondal34@gmail.com>
2019-04-15 17:40:03 +0530

    fast update

---
+++ Main.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+import pyautogui
+import time
+
+camera = int(input('enter the camera index '))
+face_cascade = cv2.CascadeClassifier('face_data.xml')
+eye_cascade = cv2.CascadeClassifier('eyes_data.xml')
+cap = cv2.VideoCapture(camera)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg2 = cv2.createBackgroundSubtractorMOG2()
+start = time.time() + 1
+x1, y1, w1, h2 = 0,10,10,10
+flag = False
+while True:
+    flag = False
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        [x1, y1, w1, h2] = [x,y,w,h]
+        flag = True
+    width = cap.get(3) 
+    height = cap.get(4)
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    frameY = y1//2 + y1//4
+    preimg = fgmask[0:int(frameY), 0:int(width)]
+    ret, frame = cap.read()
+    fgmask2 = fgbg.apply(frame)
+    fgmask2 = cv2.morphologyEx(fgmask2, cv2.MORPH_OPEN, kernel)
+    crrimg = fgmask2[0:int(frameY), 0:int(width)]
+    diff = cv2.absdiff(preimg, crrimg)
+    diffsum = diff.sum() 
+    #cv2.imshow('frame',fgmask)
+    #cv2.imshow('frame',fgmask2)
+    cv2.putText(diff,str(diffsum), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+    cv2.imshow('diff',diff)
+    cv2.imshow('frame',frame)
+    #need to                 | remove if don't work
+    if diffsum//100000 > 0 and flag:
+        if (int(time.time()-start) > 1):
+            
+            leftimg = diff[0:int(frameY), 0:int(x1 + w1//2)]
+            rightimg = diff[0:int(frameY), int(x1 + w1//2):int(width)]
+            if leftimg.sum() > rightimg.sum():
+                pyautogui.press('right')
+                print("right pressed")
+            else:
+                pyautogui.press('left')
+                print("left pressed")
+            start = time.time()
+    if cv2.waitKey(27) == ord('q'):
+        break
+    
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,10 @@
+# PC-wIth-gestures
+## OpenCV / Python
+A python script for a coding competition
+It uses openCV library to feed user's hand movement and press left or right key.
+Just ignore controller.py, testing.py,OpenCVTest.py and OpenCVTest3.py.
+
+Happy coding :)
+
+see this youtube video to know more:
+https://youtu.be/lRmVR6eZAJ0
